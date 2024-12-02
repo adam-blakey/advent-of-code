e11f8396cc9b860f6c18cd142b8228695e1dad83
Adam Blakey <adam@blakey.family>
2024-12-02 13:49:33 +0000

    2:2 complete
    In probably the most horrific way possible...

--- 2024/day_2/main.py
+++ 2024/day_2/main.py
@@ -1,22 +1,21 @@
-def is_report_safe(report):
-  levels = report.strip('\\n').split(' ')
-  prev_no = int(levels[0])
+def are_levels_safe(level1, level2, increasing):
+  if increasing:
+    if level1 <= level2:
+      return False
+  else:
+    if level1 >= level2:
+      return False
+
+  if abs(level1 - level2) > 3:
+    return False
+  
+  return True
+
+def is_report_safe(levels):
   increasing = int(levels[0]) < int(levels[1])
   for i in range(1, len(levels)):
-    if increasing:
-      if int(levels[i]) < prev_no:
-        return False
-    else:
-      if int(levels[i]) > prev_no:
-        return False
-
-    if (int(levels[i]) - prev_no) == 0:
+    if not are_levels_safe(int(levels[i]), int(levels[i-1]), increasing):
       return False
-
-    if abs(int(levels[i]) - prev_no) > 3:
-      return False
-    
-    prev_no = int(levels[i])
   
   return True
 
@@ -24,13 +23,19 @@ def main(file):
   no_safe = 0
 
   with open(file) as f:
-    for report in f:
-      if is_report_safe(report):
-        no_safe += 1
+    for report_raw in f:
+      cleaned_report_raw = report_raw.strip('\\n').split(' ')
+      for j in range(len(cleaned_report_raw)):
+        report = cleaned_report_raw.copy()
+        report.pop(j)
+
+        if is_report_safe(report):
+          no_safe += 1
+          break
   
   return no_safe
 
-assert main('test.dat') == 2
+assert main('test.dat') == 4
 print(main('input.dat')) 
 
   
